cadf7f9bb46db23ae15d4b4716b767ef19e5d7d1
Fran <f.x.rodriguez@hotmail.com>
2019-01-28 19:57:17 +0000

    bitbobs storybook improvements

--- packages/bitbobs/.storybook/config.js
+++ packages/bitbobs/.storybook/config.js
@@ -1,7 +1,14 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from "@storybook/react";
+import { setOptions, withOptions } from "@storybook/addon-options";
+
+addDecorator(
+  withOptions({
+    name: "cryptuff/bitbobs"
+  })
+);
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../src', true, /.stor(y|ies).(jsx?|tsx?)$/);
+const req = require.context("../src", true, /.stor(y|ies).(jsx?|tsx?)$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
