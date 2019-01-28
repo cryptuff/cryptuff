fcd4b3d9c1866c95d760675495d965b2195cdb5f
Fran <f.x.rodriguez@hotmail.com>
2019-01-28 16:07:15 +0000

    WIP - before ejecting moonlander

--- packages/bitbobs/README.md
+++ packages/bitbobs/README.md
@@ -1,2 +1,9 @@
 # bitbobs
+
 Bits &amp; bobs (aka components) for trading related stuff
+
+## Development workflow
+
+To run storybook:
+
+`packages/bitbobs$ npm run storybook`
--- packages/moonlander/src/App.tsx
+++ packages/moonlander/src/App.tsx
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { ITickerRequest, ITickerResponse } from "@core";
 
 class App extends Component {
   render() {
