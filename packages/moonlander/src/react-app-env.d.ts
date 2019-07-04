4e203e2f14ccf3a0a38e75cb35204809f4b07c72
spcfran <f.x.rodriguez@hotmail.com>
2019-07-04 19:51:06 +0100

    .env typing and tidy up

--- packages/moonlander/src/react-app-env.d.ts
+++ packages/moonlander/src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    KRAKEN_ENDPOINT: string;
+    ROUTER_ENDPOINT: string;
+    REACT_APP_KRAKEN_API_KEY: string;
+    REACT_APP_KRAKEN_API_SECRET: string;
+    PORT: number;
+  }
+}
--- packages/pds/src/index.ts
+++ packages/pds/src/index.ts
@@ -2,11 +2,12 @@ import dotenv from "dotenv";
 import path from "path";
 import { Connection, Session } from "autobahn";
 import { PDSServer } from "./pdsServer";
+import { KrakenRestClient } from "@cryptuff/core";
 
 dotenv.config();
 
 if (!process.env.ROUTER_ENDPOINT) throw new Error("No Router Endpoint");
-if (!process.env.KRAKEN_ENDPOINT) throw new Error("No Kraken Endpoint");
+if (!process.env.KRAKEN_WS_ENDPOINT) throw new Error("No Kraken Endpoint");
 
 var routerUrl = process.env.ROUTER_ENDPOINT;
 var realm = "com.cryptuff";
@@ -14,10 +15,15 @@ var topic = "com.cryptuff.pds";
 var getTickerProcedure = "com.cryptuff.pds.getTicker";
 
 async function run() {
-  var pdsServer = new PDSServer(routerUrl);
-  await pdsServer.connectToExchanges();
-  await pdsServer.connectToRouter();
+  // var pdsServer = new PDSServer(routerUrl);
+  // await pdsServer.connectToExchanges();
+  // await pdsServer.connectToRouter();
+  // var kraken = new KrakenRestClient();
+  // kraken.setKey(process.env.REACT_APP_KRAKEN_API_KEY!);
+  // kraken.setSecret(process.env.REACT_APP_KRAKEN_API_SECRET!);
+  // const balance = await kraken.getBalance();
+  // console.log(balance);
 }
 
-console.log("PDS is noop");
-// run();
+// console.log("PDS is noop");
+run();
