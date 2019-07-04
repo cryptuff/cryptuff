8c3da1305ace59cc2cdf25076b643eae0ccdd00e
spcfran <f.x.rodriguez@hotmail.com>
2019-07-04 19:51:48 +0100

    remove cors package and setupProxy.js

--- packages/moonlander/src/setupProxy.js
+++
@@ -1,7 +0,0 @@
-// const proxy = require('http-proxy-middleware');
-var cors = require("cors");
-
-module.exports = function(app) {
-  // app.use(proxy('/kraken', { target: 'https://api.kraken.com/' }));
-  app.use(cors());
-};
