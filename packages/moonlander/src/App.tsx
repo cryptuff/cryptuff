ed948b25e7ea94e836ce6e099db1608156b48e1a
spcfran <f.x.rodriguez@hotmail.com>
2019-07-09 20:09:26 +0100

    Experimenting with @reach/router

--- packages/moonlander/src/App.tsx
+++ packages/moonlander/src/App.tsx
@@ -3,7 +3,8 @@ import "./App.scss";
 import { Button } from "@cryptuff/bitbobs";
 import { KrakenWSClient, KrakenRestClient } from "@cryptuff/core";
 import React, { Component, Suspense } from "react";
-import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
+// import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
+import { Router, Link as NavLink, LinkProps, RouteComponentProps } from "@reach/router";
 import styled from "styled-components";
 
 import { PDSClient } from "./client";
@@ -11,7 +12,7 @@ import { KrakenTradesRig } from "./rigs/tradesRig";
 import { OrderBookRig } from "./rigs/obRig";
 import { KrakenAssetsRig } from "./rigs/assetsRig";
 import { KrakenBalanceRig } from "./rigs/balanceRig";
-import { KrakenTickersRig } from './rigs/tickersRig';
+import { KrakenTickersRig } from "./rigs/tickersRig";
 
 const pdsClient = new PDSClient();
 const kWSClient = new KrakenWSClient({ beta: false });
@@ -21,16 +22,36 @@ kRestClient.setSecret(process.env.REACT_APP_KRAKEN_API_SECRET);
 // NOTE: Run Chrome canary with --disable-web-security --disable-gpu --user-data-dir=~/chromeTemp
 // const kRestClient = new KrakenRestClient('https://cors-anywhere.herokuapp.com/https://api.kraken.com');
 
-const views = ["/trades", "/ob", "/assets", "/tickers", "/balance"];
+const views = ["/trades", "/ob", "/tickers", "/assets", "/balance"];
 
-interface State {}
+const withReach = <P extends object>(Component: React.ComponentType<P>) =>
+  Component as (typeof Component) &
+    React.ComponentType<React.ComponentProps<typeof Component> & RouteComponentProps>;
 
-class App extends Component<{}, State> {
+const KrakenTrades = withReach(KrakenTradesRig);
+const KrakenAssets = withReach(KrakenAssetsRig);
+const OrderBook = withReach(OrderBookRig);
+const KrakenBalance = withReach(KrakenBalanceRig);
+const KrakenTickers = withReach(KrakenTickersRig);
+
+class App extends Component {
   render() {
     return (
       <div className="App">
         <Suspense fallback={<h2>Loading</h2>}>
-          <BrowserRouter>
+          {views.map((view, i) => (
+            <Link key={view} to={view} className="nav">
+              {view}
+            </Link>
+          ))}
+          <Router>
+            <KrakenTrades client={kWSClient} maxNumberOfTrades={30} path="/trades" />
+            <OrderBook client={kWSClient} maxNumberOfEntries={10} path="/ob" />
+            <KrakenTickers client={kRestClient} path="/tickers" />
+            <KrakenAssets client={kRestClient} path="/assets" />
+            <KrakenBalance client={kRestClient} path="/balance" />
+          </Router>
+          {/* <BrowserRouter>
             <Button onClick={() => pdsClient.init()}>Init PDS client (open console)</Button>
             {views.map((view, i) => (
               <Link key={view} to={view} activeClassName="active" className="nav">
@@ -50,14 +71,26 @@ class App extends Component<{}, State> {
               <Route path="/balance" render={() => <KrakenBalanceRig client={kRestClient} />} />
               <Route path="/tickers" render={() => <KrakenTickersRig client={kRestClient} />} />
             </Switch>
-          </BrowserRouter>
+          </BrowserRouter> */}
         </Suspense>
       </div>
     );
   }
 }
 
-const Link = styled(NavLink)`
+const Link = styled<React.FC<LinkProps<any>>>(props => (
+  //@ts-ignore
+  <NavLink
+    {...props}
+    getProps={({ isCurrent }) => {
+      // the object returned here is passed to the
+      // anchor element's props
+      return {
+        className: props.className + " " + (isCurrent ? "active" : ""),
+      };
+    }}
+  />
+))`
   display: inline-block;
   padding: 5px;
   width: 100px;
